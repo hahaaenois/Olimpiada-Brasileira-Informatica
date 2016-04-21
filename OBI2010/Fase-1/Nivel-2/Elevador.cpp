a5d9ca8d63c2b816efaa3c5894635f4a87142dfd
hahaaenois <hahaaenois@gmail.com>
2016-04-21 19:10:39 -0300

    Elevador.cpp

---
+++ OBI2010/Fase-1/Nivel-2/Elevador.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int n,c,x,y,capacidade = 0;
+    bool teve = false;
+    scanf("%d %d", &n, &c);
+
+    for(int i = 0; i < n ; i++){
+        scanf("%d %d", &x, &y);
+        capacidade += -1*x + y;
+        if(capacidade > c){
+            teve = true;
+            break;
+        }
+    }
+
+    if(teve){
+        printf("S");
+    }else{
+        printf("N");
+    }
+
+    return 0;
+}
